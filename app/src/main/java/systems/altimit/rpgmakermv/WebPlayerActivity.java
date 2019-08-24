aaa99383f656904a88273017e8e0785956ec519e
YMNG <onlimono@gmail.com>
2019-08-25 00:38:55 +0900

    Update WebPlayerActivity.java

--- app/src/main/java/systems/altimit/rpgmakermv/WebPlayerActivity.java
+++ app/src/main/java/systems/altimit/rpgmakermv/WebPlayerActivity.java
@@ -26,7 +26,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.util.Base64;
 import android.view.View;
 import android.webkit.JavascriptInterface;
